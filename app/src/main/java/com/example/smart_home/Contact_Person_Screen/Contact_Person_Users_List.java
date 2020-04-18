75f01ccb808a5c8f71dee457cac3f665dd2afc0d
fariah zahid <fariahzahid@gmail.com>
2020-04-18 18:26:03 +0200

    Add Button Design

--- app/src/main/java/com/example/smart_home/Contact_Person_Screen/Contact_Person_Users_List.java
+++ app/src/main/java/com/example/smart_home/Contact_Person_Screen/Contact_Person_Users_List.java
@@ -2,6 +2,8 @@ package com.example.smart_home.Contact_Person_Screen;
 
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,9 +12,22 @@ import com.example.smart_home.R;
 
 public class Contact_Person_Users_List extends AppCompatActivity {
 
+    Toolbar mToolbar;
+    ListView mListView;
+    String[] countryNames = {"1","2","3","4","5"};
+    int[] countryFlags = {R.drawable.flag1,
+            R.drawable.flag2,
+            R.drawable.flag3,
+            R.drawable.flag4,
+            R.drawable.flag5
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_person_user_list);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mListView = (ListView) findViewById(R.id.listview);
+
     }
 }
---
+++ app/src/main/java/com/example/smart_home/Contact_Person_Screen/MyAdapter.java
@@ -0,0 +1,38 @@
+package com.example.smart_home.Contact_Person_Screen;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+import com.example.smart_home.R;
+
+public class MyAdapter extends ArrayAdapter<String> {
+
+    String [] names;
+    int [] flags;
+    Context mcontext;
+
+    public MyAdapter(@NonNull Context context, String[] countryNames,int[] countryFlags) {
+
+        super(context, R.layout.listview_item);
+        this.names = countryNames;
+        this.flags = countryFlags;
+        this.mcontext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return names.length;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater mInflater = (LayoutInflater) mcontext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = mInflater.inflate(R.layout.listview_item,parent,false);
+        return super.getView(position,convertView,parent);
+    }
+}
