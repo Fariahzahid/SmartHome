260f4aa1c182a71e5b453a3672b605ddca7406ff
fariah zahid <fariahzahid@gmail.com>
2020-04-18 02:15:48 +0200

    Add BottomNavigationBar

--- app/src/main/java/com/example/smart_home/Contact_Person_Screen/Contact_Person_Home.java
+++ app/src/main/java/com/example/smart_home/Contact_Person_Screen/Contact_Person_Home.java
@@ -2,55 +2,76 @@ package com.example.smart_home.Contact_Person_Screen;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
-import com.example.smart_home.Contact_Person_Fragments.Contact_Person_Notifications_Fragments;
+import com.example.smart_home.MainActivity;
 import com.example.smart_home.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import com.example.smart_home.Contact_Person_Fragments.Contact_Person_Home_Fragment;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 
 public class Contact_Person_Home extends AppCompatActivity {
-
-    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_person_home);
 
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-        openFragment(Contact_Person_Home_Fragment.newInstance("", ""));
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        //bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+        //bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_SELECTED);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
 
-    }
-    public void openFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
+        Button user_list = (Button) findViewById(R.id.contact_person_user_list_button);
+        Button add_new_user = (Button) findViewById(R.id.contact_person_new_user_button);
 
-    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    switch (item.getItemId()) {
-                        case R.id.navigation_home:
-                            openFragment(Contact_Person_Home_Fragment.newInstance("", ""));
-                            return true;
+        user_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Contact_Person_Home.this, Contact_Person_Users_List.class);
+                startActivity(intent);
 
-                        case R.id.navigation_notifications:
-                            openFragment(Contact_Person_Notifications_Fragments.newInstance("", ""));
-                            return true;
-                    }
-                    return false;
+            }
+        });
+        add_new_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Contact_Person_Home.this, Contact_Person_Add_New_Users.class);
+                startActivity(intent);
+
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.navigation_home:
+
+                        break;
+
+                    case R.id.navigation_notifications:
+                        Intent intent2 = new Intent(Contact_Person_Home.this, Contact_Person_Notification.class);
+                        startActivity(intent2);
+                        break;
+
+
                 }
-            };
+
+
+                return false;
+            }
+        });
+
+
+    }
 }
--- app/src/main/java/com/example/smart_home/Contact_Person_Screen/Contact_Person_Users_List.java
+++ app/src/main/java/com/example/smart_home/Contact_Person_Screen/Contact_Person_Users_List.java
@@ -1,38 +1,18 @@
 package com.example.smart_home.Contact_Person_Screen;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.smart_home.R;
 
-public class Contact_Person_Users_List extends Fragment {
 
-    public Contact_Person_Users_List() {
-    }
+public class Contact_Person_Users_List extends AppCompatActivity {
 
-    public static Contact_Person_Users_List newInstance(String param1, String param2) {
-        Contact_Person_Users_List fragment = new Contact_Person_Users_List();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_contact_person_home, container, false);
-        return view;
+        setContentView(R.layout.activity_contact_person_user_list);
     }
 }
